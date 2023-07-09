1fe53042809382ceb48d24e0b5dc2749d72097f6
Youssef_Saad <6270@holbertonstudents.com>
2023-07-09 20:14:17 +0100

    is positive?, is it negative or is it Zero , adding a comment

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+
 import random
+
+# Generate a random signed number
 number = random.randint(-10, 10)
-#Checks if the number is either positive, negative, or zero
+
+# Checks if the number is either positive, negative, or zero
 if number > 0:
-	print(f"{number} is positive")
+    print(f"{number} is positive")
 elif number == 0:
-	print(f"{number} is zero") 
-else: 
-	print(f"{number} is negative")
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
+	
