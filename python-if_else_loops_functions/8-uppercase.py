9c3043c9c7ba728a7e0c50ce5e697bf70777fc4c
Youssef_Saad <6270@holbertonstudents.com>
2023-07-19 17:17:54 +0100

    prints a string in uppercase, Refactor: refactoring code

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-	result = ""
-	for c in str:
-		if ord('a') <= ord(c) <= ord('z'):
-			result += chr(ord(c) - 32)
+	for char in str:
+		if ord('a') <= ord(char) <= ord('z'):
+			uppercase_char = chr(ord(char) - 32)
 		else:
-			result += c
-	print("{}".format(result))
+			uppercase_char = char
+		print(uppercase_char, end='')
+	print()
