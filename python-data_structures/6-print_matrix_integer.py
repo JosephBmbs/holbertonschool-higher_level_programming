29539ae8c689c14dda6a313254fd05a914fc8278
Youssef_Saad <6270@holbertonstudents.com>
2023-07-20 17:59:51 +0100

    Lists of lists = Matrix, Refactor: refactoring the script

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-
-def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" ")
-        print()
+def no_c(my_string):
+    return ("".join(c for c in my_string if c not in "Cc"))
