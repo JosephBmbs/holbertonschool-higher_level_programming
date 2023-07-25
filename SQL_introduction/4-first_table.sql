47182b79ea1e68b473363fa2b40e723f6fdc4044
Youssef_Saad <6270@holbertonstudents.com>
2023-07-25 17:52:20 +0100

    First table, Chor: changing script

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,8 +1,4 @@
 -- SQL script to create the 'first_table' table in the specified database.
 
-USE `hbtn_0c_0`;
-
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS first_table (id INT,
+name VARCHAR(256));
