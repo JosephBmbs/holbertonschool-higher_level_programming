dd4763a5346c5eea5f88830a1692883606a7263f
Youssef_Saad <6270@holbertonstudents.com>
2023-07-26 17:50:36 +0100

    Root user

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- a script that creates the MySQL server user user_0d_1.
+
+-- Create user user_0d_1 if not exists and set the password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
