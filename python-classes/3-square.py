5780088747d19cd4cf63e2bcb69d0dd281a5595d
Youssef_Saad <6270@holbertonstudents.com>
2023-07-29 15:58:37 +0100

    Area of a square, Fix: fixing the pycodestyle

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -4,11 +4,13 @@
 A class that defines a square
 """
 
+
 class Square:
     """Initializes a Square instance with a given size"""
 
     def __init__(self, size=0):
         """__size (int): The size of the square (private)"""
+
         if isinstance(size, int) and size >= 0:
             self.__size = size
         elif not isinstance(size, int):
@@ -18,4 +20,5 @@ class Square:
 
     def area(self):
         """define area method, evaluate square area"""
+
         return self.__size ** 2
