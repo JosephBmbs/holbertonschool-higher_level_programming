a116732712dc361e6439eb2a2b8cee418bc7bfa3
Youssef_Saad <6270@holbertonstudents.com>
2023-08-04 00:04:58 +0100

    Filter states,Chor: refactoring script

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,28 +1,10 @@
 #!/usr/bin/python3
-"""
-Script that lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa.
-"""
-
+"""Lists all states with names start with N from the database hbtn_0e_0_usa"""
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-    if len(argv) != 4:
-        print("Usage: ./1-filter_states.py <mysql_username> <mysql_password> <database_name>")
-    else:
-        username, password, database = argv[1:]
-
-
-        conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
-        cur = conn.cursor()
-
-        query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-
-        cur.execute(query)
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-        cur.close()
-        conn.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY id ASC")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
