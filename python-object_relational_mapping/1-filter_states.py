cd9de226e32fba572355f06a21adeb66d9decea8
Youssef_Saad <6270@holbertonstudents.com>
2023-08-03 01:17:47 +0100

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
-"""A script that lists all states with a name starting with N"""
+"""
+Script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa.
+"""
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-        )
-    cur = db.cursor()
-    cur.execute("SELECT * FROM the states ORDER BY states.id")
-    rows = cur.fetchall()
-    for row in rows:
-        if row[1].startswith("N"):
+    if len(argv) != 4:
+        print("Usage: ./1-filter_states.py <mysql_username> <mysql_password> <database_name>")
+    else:
+        username, password, database = argv[1:]
+
+
+        conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+        cur = conn.cursor()
+
+        query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+
+        cur.execute(query)
+        query_rows = cur.fetchall()
+        for row in query_rows:
             print(row)
-        else:
-            pass
-    cur.close()
-    db.close()
+        cur.close()
+        conn.close()
